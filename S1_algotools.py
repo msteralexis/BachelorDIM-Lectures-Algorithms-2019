85be292598dcaed981dd6c7e71f676cead753ce4
Alexis ESCUDERO <escudera@iut-acy.local>
2019-09-26 17:05:53 +0200

    ajout de la fonction max_value ainsi que les teste associé

--- S1_algotools.py
+++ S1_algotools.py
@@ -8,20 +8,24 @@ Created on Thu Sep 26 14:17:13 2019
 print (' Programme Session 1 / Alexis Escudero')
 
 
-''' # réponse au question 
-# question 1 : 
-    # cela nous métra rien 
-" question 2: 
-    # si divisions par zéro ou nombre négatif alors sa plante 
-'''
+ ## réponse au question 
+## question 1 : 
+    ## cela nous métra rien 
+## question 2: 
+    ## si divisions par zéro ou nombre négatif alors sa plante 
 
 
-'''
-# Dans cette fonction on souhaite effectuer la somme d'un tableau en paramètre par valeur 
-# on aura un tableau deux variables sont créées (une pour la somme des nombres du tableau l'autre pour le nombre total des nombres contenu dans le tableau) 
-# une condition vient vérifier si les nombres sont strictement bien positif
-'''
-def average_above_zero(table:list):
+
+
+## Dans cette fonction on souhaite effectuer la somme d'un tableau en paramètre par valeur 
+## on aura un tableau deux variables sont créées (une pour la somme des nombres du tableau l'autre pour le nombre total des nombres contenu dans le tableau) 
+## une condition vient vérifier si les nombres sont strictement bien positif
+## Je test si on envoie bien un tableau à la fonction sinon on retourn un message d'erreur 
+def average_above_zero(table):
+    
+    if not(isinstance(table, list)):
+        raise ValueError( ' il faut passer une liste') 
+        
     som = 0
     n = 0
     for i in range(len(table)):
@@ -34,7 +38,23 @@ def average_above_zero(table:list):
         return som/n
 
 
-#test  average_above_zero
+
+def max_value(table):
+    if not(isinstance(table, list)):
+        raise ValueError( ' il faut passer une liste') 
+    valeur_max=table[0]
+    indice=0
+    for i in range(len(table) ):
+        if (tab[i]> valeur_max):
+            valeur_max=tab[i]
+            indice=i
+    return(valeur_max,indice)
+
+
+''' partie teste '''
+
+##########################################
+##test  average_above_zero
 tab=[1,2,3,4,5,6]
 som=average_above_zero(tab)
 if som==0:
@@ -42,3 +62,23 @@ if som==0:
 else: 
     print ('la somme vault ', som)
 
+
+
+##########################################
+##test function max_value
+
+## Test valeur max pour chiffre négatif  
+tab=[-5,-8]
+valeur_max,indice=max_value(tab)
+print('valeur max = ',valeur_max)
+print('indice valeur = ',indice)
+
+## Test valeur max pour chiffre positif
+tab=[1,2,3,4,5,6]
+valeur_max,indice=max_value(tab)
+print('valeur max = ',valeur_max)
+print('indice valeur = ',indice)
+
+## Test si on lui envoi bien un tableau
+##tab=12
+##valeur_max,indice=max_value(tab)
