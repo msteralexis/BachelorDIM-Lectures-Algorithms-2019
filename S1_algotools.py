3e3660dfe78ac2e05009229b79512e3ff3b1fc0f
Alexis ESCUDERO <escudera@iut-acy.local>
2019-09-27 09:01:22 +0200

    réorganisation du code

--- S1_algotools.py
+++ S1_algotools.py
@@ -7,15 +7,15 @@ Created on Thu Sep 26 14:17:13 2019
 
 print (' Programme Session 1 / Alexis Escudero')
 
+##########################################
+##          Exo 1
+##########################################
 
  ## réponse au question 
 ## question 1 : 
     ## cela nous métra rien 
 ## question 2: 
     ## si divisions par zéro ou nombre négatif alors sa plante 
-
-
-
 
 ## Dans cette fonction on souhaite effectuer la somme d'un tableau en paramètre par valeur 
 ## on aura un tableau deux variables sont créées (une pour la somme des nombres du tableau l'autre pour le nombre total des nombres contenu dans le tableau) 
@@ -36,6 +36,20 @@ def average_above_zero(table):
         return 0
     else: 
         return som/n
+    
+##test  average_above_zero
+tab=[1,2,3,4,5,6]
+som=average_above_zero(tab)
+if som==0:
+    print('erreur que des nombre négatifs')
+else: 
+    print ('la somme vault ', som)
+
+
+
+##########################################
+##          Exo 2
+##########################################
 
 
 ## Dans cette fonction on passe par valeur un tableau
@@ -52,21 +66,6 @@ def max_value(table):
             indice=i
     return(valeur_max,indice)
 
-
-''' partie teste '''
-
-##########################################
-##test  average_above_zero
-tab=[1,2,3,4,5,6]
-som=average_above_zero(tab)
-if som==0:
-    print('erreur que des nombre négatifs')
-else: 
-    print ('la somme vault ', som)
-
-
-
-##########################################
 ##test function max_value
 
 ## Test valeur max pour chiffre négatif  
@@ -84,3 +83,11 @@ print('indice valeur = ',indice)
 ## Test si on lui envoi bien un tableau
 ##tab=12
 ##valeur_max,indice=max_value(tab)
+
+
+
+
+
+
+
+
