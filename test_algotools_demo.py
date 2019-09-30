f09985c8e870e060624cbc75438f5f0eb80bf582
Alexis ESCUDERO <escudera@iut-acy.local>
2019-09-30 09:32:35 +0200

    test premier test fait par alexis

--- test_algotools_demo.py
+++ test_algotools_demo.py
@@ -7,6 +7,7 @@ Created on Sun Sep 29 21:44:54 2019
 """
 
 import pytest
+import S1_algotools
 
 def inc_(x):
     return x+1
@@ -17,3 +18,13 @@ def test_inc():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         1/0
+        
+##########################################
+##          Exo 1
+##########################################
+        
+def test_exo1_1()
+    tab=[1,2,3,4,5,6]
+    som=S1_algotools.average_above_zero(tab)
+    assert som==21
+    
