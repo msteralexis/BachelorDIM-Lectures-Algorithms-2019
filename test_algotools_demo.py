7e8f83f8a114da977b8c894251ea5e74ecfbbef3
runner <benoit.alexandre.vision@gmail.com>
2019-09-30 02:34:47 -0500

    baby test 1

--- test_algotools_demo.py
+++ test_algotools_demo.py
@@ -5,8 +5,15 @@ Created on Sun Sep 29 21:44:54 2019
 
 @author: alben
 """
-
+import S1_algotools_teacherdemo as tobetested
 import pytest
+
+
+def test_average_above_zero_working1():
+	tab_list=[1,2,3,-4,6,-9]
+	test, lastID=tobetested.average_above_zero(tab_list)
+	assert test==3
+	
 
 def inc_(x):
     return x+1
