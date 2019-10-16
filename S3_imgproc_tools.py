8660b4059ab008dff547a03a99fa9c9ad3d9e198
escudero <alexis.escudero195@gmail.com>
2019-10-17 00:21:27 +0200

    ajouts debut s3 et test associée

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -7,49 +7,115 @@ Created on Mon Sep 30 13:54:10 2019
 
 print (' Programme Traitement Image / Alexis Escudero !!')
 
-import cv2 
-import numpy as np 
+import cv2
+import numpy as np
+import time
 
-#import image 
-img_gray=cv2.imread("exoquatre.png",0) 
-img_bgr=cv2.imread("exocinq.png",1) 
-img=cv2.imread("exocinq.png") 
-
-
-cv2.imshow("BGR image", img)
-cv2.waitKey()
-#dpermet de connaitre la tailel d'une image 
-print("Gray levels image shape = "+str(img_gray.shape))
-print("BGR image shape = "+str(img_bgr.shape))
-## renvoi le nombre de ligne, colone et couleur
+tmps1=time.clock()
+######################################################################
+##                  Exercice 1 / Sans fonction prédefinie
+######################################################################
 
 
-
-
-## faire un bloque fonctionelle avec ca  + TEST
-
-##inverse une image 
-for row in range(img.shape[0]):
-    for col in range (img.shape[1]):
-        for cha in range(img.shape[2]):
-            img[row,col,cha]=255-img[row,col,cha]
-
-## autre opération possible 
-img=255-img
+## inverse les couleur d'une image colorer 
+## Dans cette fonction on nous envoie un tableau 3d représentant les couleurs primaire
+## rouge,bleu,vert pour un pixel. Nous devrons donc avoir 3 boucle pour parcourire tous les "coordoner"
+## des coulurs d'un pixels pour l'inverser.
+def invert_colors_manual(img):
+    for row in range(img.shape[0]):
+        for col in range (img.shape[1]):
+            for cha in range(img.shape[2]):
+                img[row,col,cha]=255-img[row,col,cha]
+    return img
 
 
 
 
+######################################################################
+##                  Exercice 2  / NUMPY
+######################################################################
+## inverse les couleur avec la fonction près définie de Numpy
+def invert_colors_numpy(img):
+    img=255-img
+    return img
+
+
+######################################################################
+##                  Exercice 3   / CV
+######################################################################
+## inverse les couleur avec la fonction près définie de OpenCV2
+
+def  invert_colors_opencv(input_img):
+    return cv2.bitwise_not(input_img)
+   
+ 
+    
+
+def test_image(image,fonction):
+    pixel_test=255-image[0,0]
+    if(fonction ==0 ):
+        image_inverse=  invert_colors_manual(image)
+    if(fonction ==1 ):
+        image_inverse= invert_colors_numpy(image) 
+    if(fonction ==2 ):
+       image_inverse= invert_colors_opencv(image)
+    if (pixel_test.all()==image[0,0].all()):
+        return 0
+    else :
+        return 1
+
+
+    
+def test_pixel(image,image_inverser):
+    pixel_test=255-image[0,0]
+    if (pixel_test.all()==image_inverser[0,0].all()):
+        return 0
+    else :
+        return 1
+    
+    
+  ## fonction permetant de regourper en 1 test toutes les fonctions  
+def test_image2(image):
+    i=0
+    image_inverse= invert_colors_manual(image)
+    if( test_pixel(image,image_inverse)==0):
+       i=i+1
+    image_inverse= invert_colors_numpy(image) 
+    if( test_pixel(image,image_inverse) == 0):
+       i=i+1   
+    image_inverse= invert_colors_opencv(image)
+    if( test_pixel(image,image_inverse) ==0):
+       i=i+1
+    return i
+ 
+ 
+   
+    
+
+## Sur le même modèle que le bloque fonctionelle test_image2() 
+## Nous réalisons une fonction nous servant pour le calcule de temps sur tous
+    ## les bloque fonctionelles
+ 
+def time_image(image):
+    tmps1=time.clock()
+    image_inverse= invert_colors_manual(image)
+    tmps2=time.clock()
+    
+    tmps3=time.clock()
+    image_inverse= invert_colors_numpy(image) 
+    tmps4=time.clock()
+    
+    tmps5=time.clock()
+    image_inverse= invert_colors_opencv(image)
+    tmps6=time.clock()
+    
+    return (tmps2 - tmps1),(tmps4 - tmps3) , (tmps6 - tmps5)
 
 
 
-## permet d'afficher une image 
-cv2.imshow("BGR image", img)
-cv2.waitKey()
 
 
 
-#display the loaded images
-cv2.imshow("Gray levels image", img_gray)
-cv2.imshow("BGR image", img_bgr)
+
+
 cv2.waitKey()
---
+++ test_s3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 22:26:40 2019
+
+@author: alexis
+"""
+
+import pytest
+import S3_imgproc_tools as S3
+import numpy as np
+import cv2
+
+
+
+## on effectuent une fonction test pour chaque fonction qui renverrat la couleurs inverser de l'image
+## Dans une premier temps on doit charger l'image
+## On effectue l'inversison d'une couleur sur un pixel de l'image. Sela nous servirat comme valeurs de test
+def test1():
+    image_couleur=cv2.imread("exo5.jpeg")
+    pixel_test=255-image_couleur[0,0]
+    valeur_retour=S3.invert_colors_manual(image_couleur)
+    assert pixel_test.all()==image_couleur[0,0].all()
+
+
+def test2():
+    image_couleur=cv2.imread("exo5.jpeg")
+    pixel_test=255-image_couleur[0,0]
+    valeur_retour=S3.invert_colors_numpy(image_couleur)
+    assert pixel_test.all()==image_couleur[0,0].all()
+    
+
+def test3():
+    image_couleur=cv2.imread("exo5.jpeg")
+    pixel_test=255-image_couleur[0,0]
+    valeur_retour=S3.invert_colors_opencv(image_couleur)
+    assert pixel_test.all()==image_couleur[0,0].all()
+    
+    
+## long et fastidieu nous répétont beaucoups de code avec la versions précédente.
+## Il serai par conséquent beaucoups plus judicieux d'effectuer un bloque fonctionelle appelant 
+## toutes les fonction à la fois et nous renvoyant un entier correspondant au nombre de fonction qui on réussis
+## pour ce faire nosu fessons appel à la fonction test_image du fichier S3_imgproc_tool.py  
+def test_all_function():
+    image_couleur=cv2.imread("exo5.jpeg")
+    compteur_test=S3.test_image2(image_couleur)
+    assert compteur_test==3
